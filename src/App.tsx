6398f33d3ccefc7f2f11c079ebd0a2e38926ec22
DarkSky3309 <lust3309@gmail.com>
2023-07-10 15:32:30 +0300

    ready to start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from 'react';
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Car = () => {
+    return <>
+        <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45}/>
+        <PerspectiveCamera makeDefault position={[3, 2, 5]} fov={50}/>
+
+        <color args={[0,0,0]} attach="background"/>
+        <spotLight
+            color={[1,0.25,0.7]}
+            intensity={1.5}
+            angle={0.6}
+            penumbra={0.5}
+            position={[5,5,0]}
+            castShadow
+            shadow-bias={-0.00001}
+        />
+        <spotLight
+            color={[0.14,0.5,1]}
+            intensity={2}
+            angle={0.6}
+            penumbra={0.5}
+            position={[-5,5,0]}
+            castShadow
+            shadow-bias={-0.00001}
+        />
+
+    </>
+}
+
+const App = () => {
+    return (
+        <Suspense fallback={null}>
+            <Canvas shadows>
+                <Car/>
+            </Canvas>
+        </Suspense>
+    );
 }
 
 export default App;
